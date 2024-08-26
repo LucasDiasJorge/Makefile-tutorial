dd8a66947c21bd7247cdf8f588725cc6bbc76c55
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-08-26 17:35:32 -0300

    Create main.c

---
+++ Const/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Usando a constante inteira
+    printf("O valor constante é: %d\n", VALOR_CONST);
+
+    // Usando a constante string
+    printf("O valor constante string é: %s\n", VALOR_CONST_STRING);
+
+    return 0;
+}
